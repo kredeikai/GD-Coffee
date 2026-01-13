0882613e7d3dff412fa0bf4bbcd5fa889162f98d
kredeikai <muhammadrifo58@gmail.com>
2026-01-13 22:50:11 +0700

    superadmin

--- app/Http/Controllers/Superadmin/DashboardController.php
+++ app/Http/Controllers/Superadmin/DashboardController.php
@@ -38,14 +38,14 @@ class DashboardController extends Controller
         // ======================
         // CHART DATA
         // ======================
-        $revenuePerMonth = Order::selectRaw('MONTH(created_at) as month, SUM(total_price) as total')
+        $revenuePerMonth = Order::selectRaw('strftime("%m", created_at) as month, SUM(total_price) as total')
             ->whereYear('created_at', $year)
             ->where('status', 'ready')
             ->groupBy('month')
             ->orderBy('month')
             ->get();
 
-        $orderPerMonth = Order::selectRaw('MONTH(created_at) as month, COUNT(*) as total')
+        $orderPerMonth = Order::selectRaw('strftime("%m", created_at) as month, COUNT(*) as total')
             ->whereYear('created_at', $year)
             ->groupBy('month')
             ->orderBy('month')
@@ -55,12 +55,12 @@ class DashboardController extends Controller
         // TOP MENU
         // ======================
         $topMenus = OrderItem::select(
-                'menu_id',
-                DB::raw('SUM(qty) as total_sold')
-            )
+            'menu_id',
+            DB::raw('SUM(qty) as total_sold')
+        )
             ->whereHas('order', function ($q) use ($year) {
                 $q->whereYear('created_at', $year)
-                  ->where('status', 'ready');
+                    ->where('status', 'ready');
             })
             ->groupBy('menu_id')
             ->orderByDesc('total_sold')
